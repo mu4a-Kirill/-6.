4c4a369701cc48bcc3f0e24887dba16decbd50df
mu4a_ <mu4a_@DESKTOP-4DP2NVH>
2024-09-12 09:45:03 +0300

    Добавьте файлы проекта.

---
+++ ConsoleApplication1/ConsoleApplication1.cpp
@@ -0,0 +1,23 @@
+﻿#include <iostream>
+#include <string>
+using namespace std;
+int main()
+{  //zadaniye 1
+	int int1 = 48;
+	int int2 = -19;
+	int int3 = int1 + int2;
+	int int4 = int1 - int2;
+	int int5 = int1 * int2;
+	int int6 = int1 / int2;
+	int int7 = int1 % int2;
+	cout << int3 << endl << int4 << endl << int5 << endl << int6 << endl << int7 << endl;
+
+	//zadaniye 2
+	int int8 = int1 == int2;
+	int int9 = int1 != int2;
+	int int10 = int1 > int2;
+	int int11 = int1 < int2;
+	int int12 = int1 >= int2;
+	int int13 = int1 <= int2;
+	cout << int8 << endl << int9 << endl << int10 << endl << int11 << endl << int12 << endl << int13 << endl;
+}
